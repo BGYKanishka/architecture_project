e629872191bf42a39ad14492c24de9cb56e01c10
BGYKanishka <bgykanishka@gmail.com>
2026-02-12 17:54:01 +0530

    SignupRequest Fixed

--- backend/src/main/java/com/bookfair/system/dto/request/SignupRequest.java
+++ backend/src/main/java/com/bookfair/system/dto/request/SignupRequest.java
@@ -4,19 +4,24 @@ import lombok.Data;
 
 @Data
 public class SignupRequest {
-    @NotBlank//(message = "Name is required")
-    //@Size(max = 100, message = "Name must be under 100 characters")
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must be under 100 characters")
     private String name;
-    @NotBlank//(message = "Email is required") 
-    @Email//(message = "Please provide a valid email address")
+    
+    @NotBlank(message = "Email is required") 
+    @Email(message = "Please provide a valid email address")
     private String email;
-    @NotBlank//(message = "Password is required")
-    @Size(min = 6)
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
-    private String role; // "vendor", "admin", "employee"
-    //@NotBlank(message = "Contact number is required")
-    //@Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Contact number must be a valid format (10-15 digits)")
+    
+    private String role; 
+    
+    @NotBlank(message = "Contact number is required")
+    @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Contact number must be a valid format (10-15 digits)")
     private String contactNumber;
-    //@Size(max = 100, message = "Business name must be under 100 characters")
+    
+    @Size(max = 100, message = "Business name must be under 100 characters")
     private String businessName;
 }
