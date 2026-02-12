486b19a8fa0dd23ab897666fdc6f64c89e3bfac8
Dheeshana De Silva <dheeshanadesilva2002@gmail.com>
2026-02-12 15:24:02 +0530

    Modify validation annotations in LoginRequest

--- backend/src/main/java/com/bookfair/system/dto/request/LoginRequest.java
+++ backend/src/main/java/com/bookfair/system/dto/request/LoginRequest.java
@@ -4,8 +4,9 @@ import lombok.Data;
 
 @Data
 public class LoginRequest {
-    @NotBlank
+    @NotBlank//(message = "Email is required")
+    //@Email(message = "Please provide a valid email address")
     private String email;
-    @NotBlank
+    @NotBlank//(message = "Password is required")
     private String password;
 }
