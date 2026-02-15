4421c0535b54d90a5ea4f44d93c027283c98ec10
Janani Wijerathne <jkwijerathne7@gmail.com>
2026-02-16 00:01:17 +0530

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,28 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/Register"; // Import this
 import Dashboard from "./pages/Dashboard";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} /> {/* Add this route */}
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        
+        <div style={{ flex: 1 }}>
+
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} /> {/* Add this route */}
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+
+        </div>
+
+        <Footer />
+        
+      </div>
     </Router>
   );
 }
